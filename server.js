481e4e4d3be70b84c6fcc25bd5331dee087a0d1c
kundan761 <kundankumarsuraj761@gmail.com>
2024-04-30 02:21:57 +0530

    cors implement

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+server.use(cors());
 
 // Endpoint to get user data by ID
 server.get('/users/:id/data', (req, res) => {
